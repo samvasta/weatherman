675c477ea17f865ce6b8bc2a026b4ea4f0bf9e27
Sam Vasta <sam@pocus.com>
2024-03-06 10:16:55 -0600

    add sim functions

--- simulator/sim/simulate.go
+++ simulator/sim/simulate.go
@@ -8,8 +8,6 @@ import (
 type ModelState struct {
 	Model *simulator.Model
 
-	PrevState *ModelState
-
 	Step int
 
 	sortedVariables []variables.Variable
@@ -30,24 +28,56 @@ func InitModelState(model *simulator.Model) ModelState {
 
 func SimulateOneStep(prevState *ModelState) ModelState {
 	inputs := make(map[string]float64)
-	if prevState.Results != nil {
-		for _, c := range prevState.Model.Collectors {
+	collectorValues := make(map[string]float64)
+
+	for _, c := range prevState.Model.Collectors {
+		if prevState.Results != nil {
 			inputs[c.Target.Name] = prevState.Results[c.Name]
 		}
+		collectorValues[c.Name] = 0
 	}
 
-	nextResults := make(map[string]float64)
 	for _, v := range prevState.sortedVariables {
 		result := v.Compute(inputs)
 		inputs[v.GetInfo().Name] = result
-		nextResults[v.GetInfo().Name] = result
+
+		if _, ok := collectorValues[v.GetInfo().Name]; ok {
+			collectorValues[v.GetInfo().Name] = result
+		}
 	}
 
 	return ModelState{
 		Model:           prevState.Model,
-		PrevState:       prevState,
 		Step:            prevState.Step + 1,
 		sortedVariables: prevState.sortedVariables,
-		Results:         nextResults,
+		Results:         collectorValues,
 	}
+}
+
+func Simulate(model *simulator.Model, steps int) []ModelState {
+	state := InitModelState(model)
+
+	states := make([]ModelState, steps+1)
+	states[0] = state
+
+	for i := 0; i < steps; i++ {
+		state = SimulateOneStep(&state)
+		states = append(states, state)
+	}
+
+	return states
+}
+
+type MonteCarloResult struct {
+	Steps []ModelState
+}
+
+func MonteCarlo(model *simulator.Model, steps int, iterations int) []MonteCarloResult {
+	runs := make([]MonteCarloResult, iterations)
+
+	for i := 0; i < iterations; i++ {
+		runs[i] = MonteCarloResult{Steps: Simulate(model, steps)}
+	}
+
+	return runs
 }
--- simulator/sim/simulate_test.go
+++ simulator/sim/simulate_test.go
@@ -1,6 +1,7 @@
 package sim
 
 import (
+	"slices"
 	"testing"
 
 	"samvasta.com/weatherman/simulator"
@@ -45,13 +46,94 @@ func TestSimulateMultiStep(t *testing.T) {
 	state := InitModelState(&model)
 
 	for i := 0; i < 10; i++ {
-		t.Logf("Step %v, results=%v", i+1, state.Results)
 		state = SimulateOneStep(&state)
 		result := state.Results["collector"]
+		t.Logf("Step %v, results=%v", i+1, state.Results)
 
 		if result != float64(i+2) {
 			t.Errorf("Expected %v, got %v", i+2, result)
 		}
 	}
+}
 
+func TestMonteCarlo(t *testing.T) {
+	model := simulator.NewModel()
+
+	initialPrinciple := variables.IVar{
+		VariableInfo: variables.VariableInfo{
+			Name: "principle",
+		},
+		Distribution: distributions.Constant{Value: 1000},
+	}
+
+	contribution := variables.IVar{
+		VariableInfo: variables.VariableInfo{
+			Name: "contribution",
+		},
+		Distribution: distributions.Constant{Value: 100},
+	}
+
+	returnOnInvestment := variables.IVar{
+		VariableInfo: variables.VariableInfo{
+			Name: "returnOnInvestment",
+		},
+		Distribution: distributions.Normal{
+			Mean:   0.1,
+			StdDev: 0.05,
+		},
+	}
+
+	totalPrinciple := variables.Sum{
+		VariableInfo: variables.VariableInfo{
+			Name: "totalPrinciple",
+		},
+		Variables: []variables.Variable{initialPrinciple, contribution},
+	}
+
+	const1 := variables.IVar{
+		VariableInfo: variables.VariableInfo{
+			Name: "const1",
+		},
+		Distribution: distributions.Constant{Value: 1},
+	}
+
+	roiMultiplier := variables.Sum{
+		VariableInfo: variables.VariableInfo{
+			Name: "roiMultiplier",
+		},
+		Variables: []variables.Variable{returnOnInvestment, const1},
+	}
+
+	finalValue := variables.Product{
+		VariableInfo: variables.VariableInfo{
+			Name: "result",
+		},
+		Variables: []variables.Variable{totalPrinciple, roiMultiplier},
+	}
+
+	collector := variables.Collector{
+		VariableInfo: variables.VariableInfo{
+			Name: "collector",
+		},
+		Input:  finalValue,
+		Target: &initialPrinciple,
+	}
+
+	model.AddCollector(collector)
+
+	runs := MonteCarlo(&model, 10, 10000)
+
+	finalValues := make([]float64, len(runs))
+	for i, run := range runs {
+		finalStep := run.Steps[len(run.Steps)-1]
+		finalValues[i] = finalStep.Results["collector"]
+		t.Logf("Run %v: %v", i, finalStep.Results["collector"])
+	}
+
+	slices.Sort(finalValues)
+	median := finalValues[len(finalValues)/2]
+
+	if median < 4300 || median > 4400 {
+		t.Errorf("Median final value should be about 4347 but got Median=%v", median)
+	}
 }
