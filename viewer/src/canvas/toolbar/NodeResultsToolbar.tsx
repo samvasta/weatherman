42a0167a2c215897acfdbe017e01d668d11c71fc
Sam Vasta <hello@samvasta.com>
2024-03-24 01:04:28 -0500

    add chart

--- viewer/src/canvas/toolbar/NodeResultsToolbar.tsx
+++ viewer/src/canvas/toolbar/NodeResultsToolbar.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 import { useNodes } from "reactflow";
 
+import { Button } from "@/components/primitives/button/Button";
+import { Dialog } from "@/components/primitives/floating/dialog/Dialog";
+
 import { EmptyState } from "@/components/empty/EmptyState";
 
-import { AllVariables } from "@/types/variables/allVariables";
+import { RiverChart } from "@/charts/river/RiverChart";
+import { type AnyVariableData } from "@/types/variables/allVariables";
 
 import { useSimulationResultForNode } from "../atoms";
 import { type VariableNodeType } from "../useNodesAndEdges";
@@ -14,9 +18,7 @@ export function NodeResultsToolbar({
 }: {
   selected: VariableNodeType;
 }) {
-  const info = AllVariables[selected.data.type];
-
-  const nodes = useNodes();
+  const nodes = useNodes<AnyVariableData>();
 
   const node = React.useMemo(
     () => nodes.find((n) => n.id === selected.id),
@@ -24,6 +26,8 @@ export function NodeResultsToolbar({
   );
 
   const results = useSimulationResultForNode(selected.data.name);
+
+  const [type, setType] = React.useState<"hair" | "river">("river");
 
   if (!node) {
     return <EmptyState heading="Invalid Node" />;
@@ -35,7 +39,38 @@ export function NodeResultsToolbar({
 
   return (
     <div className="flex max-h-[50vh] w-fit gap-2 overflow-y-auto p-4">
-      {JSON.stringify(results.steps)}
+      {/* {JSON.stringify(results.steps)} */}
+      <Dialog
+        contentProps={{
+          className:
+            "min-h-[80vh] h-[400px] w-[80vw] max-w-[80vw] max-h-[80vh] p-0",
+        }}
+        content={({ onClose }) => (
+          <div className="relative h-[80vh] w-[80vw]">
+            <Button
+              className="absolute left-4 top-4"
+              onClick={() =>
+                setType((prev) => (prev === "river" ? "hair" : "river"))
+              }
+            >
+              toggle type
+            </Button>
+            <RiverChart
+              name={node.data.name}
+              data={{
+                baseColor: "",
+                percentiles: results.steps,
+                series: results.iterationSeries,
+              }}
+              type={type}
+            />
+          </div>
+        )}
+      >
+        <Button variant="solid" colorScheme="primary" className="w-fit">
+          River Chart
+        </Button>
+      </Dialog>
     </div>
   );
 }
