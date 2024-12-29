9e07663a1129af562ecf186813d66c679ba7d999
Sam Vasta <hello@samvasta.com>
2024-12-29 11:54:15 -0600

    update vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,11 +7,6 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
